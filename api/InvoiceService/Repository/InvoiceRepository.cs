a6d1780b747f8455a34b6eeb09bda347bed3736d
Kenneth Montealto <k.montealto@mylogoinc.com>
2022-08-05 20:37:20 +0800

    no message

--- api/InvoiceService/Repository/InvoiceRepository.cs
+++ api/InvoiceService/Repository/InvoiceRepository.cs
@@ -9,7 +9,15 @@ public class InvoiceRepository : IInvoiceRepository
     }
     public List<Invoice> FetchInvoices()
     {
-        return db.Invoices.Where(z => z.TotalAmount != null).ToList();
+        return db.Invoices.Where(z => z.TotalAmount != null).GroupBy(z => new { z.Month, z.Year })
+        .Select(z => new Invoice
+        {
+            Id = z.OrderBy(z=>z.IssuingDate).Select(x => x.Id).LastOrDefault(),
+            IssuingDate = z.OrderBy(z=>z.IssuingDate).Select(x => x.IssuingDate).LastOrDefault(),
+            Month = z.OrderBy(z=>z.IssuingDate).Select(x => x.Month).LastOrDefault(),
+            Year = z.OrderBy(z=>z.IssuingDate).Select(x => x.Year).LastOrDefault(),
+            TotalAmount = z.OrderBy(z=>z.IssuingDate).Select(x => x.TotalAmount).LastOrDefault(),
+        }).ToList();
     }
 
     public void GenerateInvoice()
